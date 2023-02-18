fee8ebea90d05f518047e269af6cb5adc62a27b5
Jayesh Kumar <jayeshmotog@gmail.com>
2023-02-18 14:13:49 -0500

    Added iPhone Frame

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { DeviceFrameset } from 'react-device-frameset'
+import '../node_modules/react-device-frameset/dist/styles/marvel-devices.min.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DeviceFrameset device="iPhone X" color="gold">
+            <div>Hello world</div>
+        </DeviceFrameset>
     </div>
   );
 }
