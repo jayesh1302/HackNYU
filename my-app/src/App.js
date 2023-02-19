5976c56392167aab5ea776c915327b482c79cfd2
Ellis <ellispinsky5@gmail.com>
2023-02-18 20:00:35 -0500

    Maps component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,11 +5,14 @@ import { DeviceFrameset } from 'react-device-frameset'
 import '../node_modules/react-device-frameset/dist/styles/marvel-devices.min.css'
 import Photo from './Photo' 
 import NavbarSmile from './Navbar-Container';
+import Maps from './Maps';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HomeComponent from './Home-Component';
 
 function App() {
   // const result = Navbar.Navbar();
   return (
+    
     <div className="App">
       <div className="container">
       <DeviceFrameset device="iPhone X" color="gold">
@@ -20,6 +23,7 @@ function App() {
         </DeviceFrameset>
     </div>
     </div>
+    
   );
 }
 
