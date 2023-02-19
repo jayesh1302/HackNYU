dc12a13ea5f7b1beeebb7846412a3f7a9293cb4b
Jayesh Kumar <jayeshmotog@gmail.com>
2023-02-18 20:41:24 -0500

    Revert "Photo Component refactored"
    This reverts commit bd31b89cae622bdc5e01139704dab6ed14ae0904.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,9 +5,6 @@ import { DeviceFrameset } from 'react-device-frameset'
 import '../node_modules/react-device-frameset/dist/styles/marvel-devices.min.css'
 import Photo from './Photo' 
 import NavbarSmile from './Navbar-Container';
-import Maps from './Maps';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import HomeComponent from './Home-Component';
 
 function App() {
   // const result = Navbar.Navbar();
@@ -18,8 +15,6 @@ function App() {
       <DeviceFrameset device="iPhone X" color="gold">
             {/* <div>Hello world</div> */}
             <NavbarSmile />
-            <HomeComponent />
-
         </DeviceFrameset>
     </div>
     </div>
--- my-app/src/Photo.js
+++ my-app/src/Photo.js
@@ -1,47 +1,15 @@
+// import pexelsPhotoByAndr from "./assets/Photo.js.png";
 import smilePhoto from "./assets/smilePhoto.png";
-import feed1 from "./assets/feeding_1.png";
-import feed2 from "./assets/feeding_2.png";
-import feed3 from "./assets/feeding_3.png";
-import './Photo.css';
-
 
 function Photo() {
-
   return (
-    <div className="photo-container">
-
-      <div className="d-flex flex-column mb-10">
-        <div className="frame">
-          <span className="lori">Lori</span>
-          <img className="smilePhoto" src={smilePhoto}/>
-          <span className="meeting-my-old-time">
-            Meeting my old time friend when wondering the neighborhood...
-          </span>
-        </div>
-        <div className="frame">
-          <span className="lori">Elaine</span>
-          <img className="smilePhoto" src={feed1}/>
-          <span className="meeting-my-old-time">
-            Meeting my old time friend when wondering the neighborhood...
-          </span>
-        </div>
-        <div className="frame">
-          <span className="lori">Alyssa</span>
-          <img className="smilePhoto" src={feed2}/>
-          <span className="meeting-my-old-time">
-            Meeting my old time friend when wondering the neighborhood...
-          </span>
-        </div>
-        <div className="frame">
-          <span className="lori">Marie</span>
-          <img className="smilePhoto" src={feed3}/>
-          <span className="meeting-my-old-time">
-            Meeting my old time friend when wondering the neighborhood...
-          </span>
-        </div>
-      </div>
+    <div className="frame-2">
+      <span className="lori">Lori</span>
+      <img className="smilePhoto" src={smilePhoto}/>
+      <span className="meeting-my-old-time">
+        Meeting my old time friend when wondering the neighborhood...
+      </span>
     </div>
   );
 }
-
 export default Photo;
